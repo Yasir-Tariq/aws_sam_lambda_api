343878d8de6598305a1cdb33741c76ce34bee8f3
Yasir-Tariq <ynaseem78@gmail.com>
2020-03-05 23:42:09 +0500

    multiple functions and separate api

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('INDEXXXX lambda function!!!!!!!!!!!')
+    };
+    callback(null, response);
+  };
+  
+  
